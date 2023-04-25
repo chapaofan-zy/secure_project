b97c5841227a3e0a3da099e00e81ef52498eae33
“chapaofan-zy” <1095004630@qq.com>
2023-04-25 10:52:09 +0800

    feat: vite打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [eslint({ cache: false }), react()],
+  base: './',
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
 });
